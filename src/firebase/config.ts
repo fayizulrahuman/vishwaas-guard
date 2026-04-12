efd8821ba78b643cdf363a077ca190b80a0ba63d
Fayizul Rahuman <fayizulrahuman2005@gmail.com>
2026-04-12 03:16:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1544949078-4a151",
+  "appId": "1:632188335725:web:d476a311698d7bddd0f164",
+  "apiKey": "AIzaSyBpTksM65uoOHRFV4TezVEWHWzz6OZijZM",
+  "authDomain": "studio-1544949078-4a151.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "632188335725"
+};
